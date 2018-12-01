1f7ffdf96c89b7d1f69f7339995c31cfc2ec542f
Letícia de Oliveira Nunes <leticia_000@users.noreply.github.com>
2018-12-01 09:22:59 -0200

    nada de novo

---
+++ sensores.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include "avr_usart.h"
+#include "bits.h"
+
+
---
+++ sensores.h
@@ -0,0 +1,12 @@
+#ifndef _SENSORES_H
+#define _SENSORES_H
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <stdio.h>
+
+
+
+
+
+#endif
