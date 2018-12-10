f6094135473679564504259d628eabafc6729f39
Hellen Avila Rosa <hellenar12@gmail.com>
2018-12-10 15:17:16 -0200

    apenas um pacote para os sensores

--- modbus.c
+++ modbus.c
@@ -33,92 +33,93 @@ uint16_t CRC16_2(uint8_t *buf, int len)
 }
 
 uint8_t modbus(uint8_t dados_s0,uint8_t dados_s1,uint8_t dados_s2,uint8_t dados_s3){
-	uint8_t pkg_s0[8] = {0x15, 0x01, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00}; //end, R/W, reg, dados, crc
-	uint8_t pkg_s1[8] = {0x15, 0x01, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00}; //end, R/W, reg, dados, crc
-	uint8_t pkg_s2[8] = {0x15, 0x01, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00}; //end, R/W, reg, dados, crc
-	uint8_t pkg_s3[8] = {0x15, 0x01, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00}; //end, R/W, reg, dados, crc
+	uint8_t pkg_sensores[8] = {0x15, 0x01, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00}; //end, R/W, reg, dados, crc
 
-	uint8_t rx_pkg_s0[16]; // resposta do modbus
-	uint8_t rx_pkg_s1[16]; // resposta do modbus
-	uint8_t rx_pkg_s2[16]; // resposta do modbus
-	uint8_t rx_pkg_s3[16]; // resposta do modbus
+	uint8_t rx_pkg_sensores[16]; // resposta do modbus
 	uint8_t i;
 	uint16_t crc;
 
 //PRIMEIRO PACOTE (s0)
 	//big_endian
-	pkg_s0[4] = dados_s0 >> 8;
-	pkg_s0[5] = dados_s0 & 0xff;
+	pkg_sensores[3] =0x05;
+	pkg_sensores[4] = dados_s0 >> 8;
+	pkg_sensores[5] = dados_s0 & 0xff;
 
-	crc = CRC16_2(pkg_s0,6);
+	crc = CRC16_2(pkg_sensores,6);
 
-	pkg_s0[6] = crc >> 8;
-	pkg_s0[7] = crc & 0xff;
+	pkg_sensores[6] = crc >> 8;
+	pkg_sensores[7] = crc & 0xff;
 
 	for (i=0; i < 8; i++)
-		USART_tx(pkg_s0[i]); //enviando para a usart todo o pacote s0
+		USART_tx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
 
 	//esperando resposta (sempre que termina de enviar ele espera a resposta, resposta por interrupção)
 	for (i=0; i < 8; i++)
-		USART_rx(pkg_s0[i]); //enviando para a usart todo o pacote s0
+		USART_rx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
 
 	_delay_ms(1000);
 
 
 //SEGUNDO PACOTE (s1)
 	//big_endian
-	pkg_s1[4] = dados_s1 >> 8;
-	pkg_s1[5] = dados_s1 & 0xff;
+	pkg_sensores[3] =0x06;
+	pkg_sensores[4] = dados_s1 >> 8;
+	pkg_sensores[5] = dados_s1 & 0xff;
 
-	crc = CRC16_2(pkg_s1,6);
+	crc = CRC16_2(pkg_sensores,6);
 
-	pkg_s1[6] = crc >> 8;
-	pkg_s1[7] = crc & 0xff;
+	pkg_sensores[6] = crc >> 8;
+	pkg_sensores[7] = crc & 0xff;
 
 	for (i=0; i < 8; i++)
-		USART_tx(pkg_s1[i]); //enviando para a usart todo o pacote s0
+		USART_tx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
 
-	//esperando resposta
+	//esperando resposta (sempre que termina de enviar ele espera a resposta, resposta por interrupção)
 	for (i=0; i < 8; i++)
-		USART_rx(pkg_s1[i]); //enviando para a usart todo o pacote s0
+		USART_rx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
 
 	_delay_ms(1000);
 
-//TERCEIRO PACOTE (s1)
+//TERCEIRO PACOTE (s2)
 	//big_endian
-	pkg_s2[4] = dados_s2 >> 8;
-	pkg_s2[5] = dados_s2 & 0xff;
+	pkg_sensores[3] =0x07;
+	pkg_sensores[4] = dados_s2 >> 8;
+	pkg_sensores[5] = dados_s2 & 0xff;
 
-	crc = CRC16_2(pkg_s1,6);
+	crc = CRC16_2(pkg_sensores,6);
 
-	pkg_s2[6] = crc >> 8;
-	pkg_s2[7] = crc & 0xff;
-
-	for (i=0; i < 8; i++)
-		USART_tx(pkg_s2[i]); //enviando para a usart todo o pacote s0
+	pkg_sensores[6] = crc >> 8;
+	pkg_sensores[7] = crc & 0xff;
 
 	for (i=0; i < 8; i++)
-		USART_rx(pkg_s2[i]);
-	//esperando resposta
+		USART_tx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
+
+	//esperando resposta (sempre que termina de enviar ele espera a resposta, resposta por interrupção)
+	for (i=0; i < 8; i++)
+		USART_rx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
 
 	_delay_ms(1000);
 
-//QUARTO PACOTE (s1)
+//QUARTO PACOTE (s3)
 	//big_endian
-	pkg_s3[4] = dados_s3 >> 8;
-	pkg_s3[5] = dados_s3 & 0xff;
+	//big_endian
+	pkg_sensores[3] =0x05;
+	pkg_sensores[4] = dados_s3 >> 8;
+	pkg_sensores[5] = dados_s3 & 0xff;
 
-	crc = CRC16_2(pkg_s3,6);
+	crc = CRC16_2(pkg_sensores,6);
 
-	pkg_s3[6] = crc >> 8;
-	pkg_s3[7] = crc & 0xff;
+	pkg_sensores[6] = crc >> 8;
+	pkg_sensores[7] = crc & 0xff;
 
 	for (i=0; i < 8; i++)
-		USART_tx(pkg_s3[i]); //enviando para a usart todo o pacote s0
+		USART_tx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
 
-	//esperando resposta
+	//esperando resposta (sempre que termina de enviar ele espera a resposta, resposta por interrupção)
 	for (i=0; i < 8; i++)
-		USART_rx(pkg_s3[i]); //enviando para a usart todo o pacote s0
+		USART_rx(pkg_sensores[i]); //enviando para a usart todo o pacote s0
+
+	_delay_ms(1000);
 
 	_delay_ms(1000);
 
